1b46e17d866d9c6f8e0df49c86035b148229256b
0s4k4 <soulbosss@gmail.com>
2020-08-11 15:47:41 -0500

    Creacion de common, web, carpeta entities, json, coneccion de base de datos

---
+++ OnSale.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OnSale.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ OnSale.Web/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OnSale.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+}
